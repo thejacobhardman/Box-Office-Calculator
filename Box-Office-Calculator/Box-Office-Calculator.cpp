182c1b707ead835c244d9e9f3551897f0da74fae
Jacob Hardman <42230736+thejacobhardman@users.noreply.github.com>
2019-09-17 13:25:25 -0600

    add program

--- Box-Office-Calculator/Box-Office-Calculator.cpp
+++ Box-Office-Calculator/Box-Office-Calculator.cpp
@@ -4,10 +4,38 @@
 // 9/17/19
 
 #include <iostream>
+#include <string>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+	string movieName;
+	int adultTickets;
+	int childTickets;
+	double adultPrice = 10.00;
+	double childPrice = 6.00;
+	double grossProfit;
+	double netProfit;
+	double distProfit;
+
+	cout << setprecision(2) << fixed;
+
+    cout << "Please enter the name of the movie that you'd like to calculate profits for: ";
+	getline(cin, movieName);
+
+	cout << "Please enter the number of adult tickets sold: ";
+	cin >> adultTickets;
+
+	cout << "Please enter the number of child tickets sold: ";
+	cin >> childTickets;
+
+	grossProfit = (adultPrice * adultTickets) + (childPrice * childTickets);
+	netProfit = grossProfit * 0.2;
+	distProfit = grossProfit - netProfit;
+
+	cout << "\n" << movieName << " had a gross profit of: $" << grossProfit;
+	cout << "\nThe theater made a profit of: $" << netProfit;
+	cout << "\nThe theater paid the movie's distributor: $" << distProfit << "\n";
 }
